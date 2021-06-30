955cb4045365846a49f327bf36b41cfb7ee8a360
Cazacu-Condart Dumitru <71759697+Cazacu-Condrat-Dumitru@users.noreply.github.com>
2021-06-30 12:32:25 +0300

    Task7 finished

---
+++ BASICS/Ex7/Task7.js
@@ -0,0 +1,17 @@
+let age = prompt('Write your age');
+
+//First Variant
+/* if (age >= 14 && age <= 90) {
+  alert('Normal age');
+}
+else {
+  alert('Not big enough!');
+} */
+
+//Second Variant
+if (age = !(age >= 14 && age <= 90)) {
+  alert('Not big enough!');
+}
+else {
+  alert('Normal age');
+}
