8189c30b23c16f4a6690426ce61fa85172ab23f2
Cazacu-Condart Dumitru <71759697+Cazacu-Condrat-Dumitru@users.noreply.github.com>
2021-07-01 13:08:15 +0300

    Task8 Finished

---
+++ BASICS/Ex8/Task8.js
@@ -0,0 +1,16 @@
+let userName = prompt('Write your username');
+let password = prompt('Write Your pasword');
+
+if (userName === 'John') {
+  alert("Welcome John");
+}
+else {
+  alert("Wrong username");
+}
+
+if (password === John123) {
+  alert("Welcome back John")
+}
+else {
+  alert("Wrong password")
+}
