e73ec95a74c51b206adff22c36860dd2f36b8a3b
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2021-07-13 15:53:44 +0300

    initial commit

---
+++ JS Syntax/Ex6/Task6.js
@@ -0,0 +1,14 @@
+let number = prompt('Write a number');
+
+if (number > 0) {
+  alert(1);
+}
+else if (number < 0) {
+  alert(-1);
+}
+else if (number == 0) {
+  alert(0);
+}
+else {
+  alert('Please write a number');
+}
