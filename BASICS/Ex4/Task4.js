69cdbafbb728501af1efe94faf2a7f5f8bf6cf7c
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2021-07-14 14:02:03 +0300

    Made with const

--- BASICS/Ex4/Task4.js
+++ BASICS/Ex4/Task4.js
@@ -1,7 +1,11 @@
 //todo use const
-function ucFirst (str){
+/*function ucFirst (str){
 
   alert(str[0].toUpperCase() + str.slice(1));
   }
   
-  ucFirst(prompt('lowercase word'));
+  ucFirst(prompt('lowercase word'));*/
+
+const phrase=`using const method`;
+
+alert(`${phrase[0].toUpperCase()}${phrase.slice(1)} `);
