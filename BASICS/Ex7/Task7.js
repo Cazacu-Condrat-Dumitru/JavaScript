9e6bc07c97c72ea52047913276c2ba04bc4afb34
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2021-07-23 16:13:45 +0300

    Finished task7

---
+++ BASICS/Ex7/Task7.js
@@ -0,0 +1,7 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function extract(start, end) {
+    return array.slice(start, end + 1);
+}
+
+console.log(extract(2, 4)); //extract(2, 4) вернет [3, 4, 5]
