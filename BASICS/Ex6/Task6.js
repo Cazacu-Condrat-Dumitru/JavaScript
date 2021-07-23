803f4bba0f7ea428be08dc0b492140d18156f8e5
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2021-07-23 11:27:14 +0300

    Finished task6

--- BASICS/Ex6/Task6.js
+++ BASICS/Ex6/Task6.js
@@ -2,4 +2,4 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log('Initial Array:', array);
 
 array.splice(array.indexOf(5), 1, 0, 0, 0); //нужно получить [1, 2, 3, 4, 0, 0, 0, 6, 7, 8, 9]
-console.log('The swapped Array: ',array);
+console.log('The swapped Array: ', array);
