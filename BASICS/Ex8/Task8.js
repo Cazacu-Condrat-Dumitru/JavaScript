5e004f47c2022c54629021c85d589b59f0a47ad8
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2021-07-29 23:27:07 +0300

    Finished task8 using function expression

--- BASICS/Ex8/Task8.js
+++ BASICS/Ex8/Task8.js
@@ -1,7 +1,12 @@
 const colors = ["red", "green", "blue"];
 
+const checkColor = function (color) {
+    return colors.indexOf(color);
+}
+/*
 function checkColor(color) {
     return colors.indexOf(color);
 }
+*/
 
-console.log(checkColor("red"));
+console.log(checkColor(`red`));
